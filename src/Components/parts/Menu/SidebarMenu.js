0d54aa003cdd2b57c4ce4d5ac7783e79ab72d765
Kalovelo <apostoloskalovelonis@ieee.org>
2019-07-06 03:59:25 +0300

    burger lines on sidebarmenu

--- src/Components/parts/Menu/SidebarMenu.js
+++ src/Components/parts/Menu/SidebarMenu.js
@@ -34,11 +34,16 @@ const SidebarMenu = () => {
     return <Col className='sideMenu' xs={1}>
         <Col className='sideMenu__insideBorderContent' xs={12}>
             <h5>LOGO</h5>
-            <a className='sideMenu__insideBorderContent__vertical-text' href="https://www.github.com/"> MENU</a>
+                <a className='v-link' href="https://www.github.com/"> MENU
+                    <div className='v-link__v-lines'>
+                        <div className='v-link__v-line-1 v-link__v-line-1--closed'/>
+                        <div className='v-link__v-line-2 v-link__v-line-2--closed'/>
+                    </div>
+                </a>
         </Col>
 
         <Col className='sideMenu__outofBorderContent sideMenu__outofBorderContent-stella' xs={12}>
-                <div className='sideMenu__outofBorderContent__colourBubble-stella'/>
+                <div className='sideMenu__outofBorderContent__colourBubble-stella'/>{/*colourBubble based on router*/}
                 <span className='sideMenu__outofBorderContent__colourCode'>{colours[0].code}</span>
                 <span className='sideMenu__outofBorderContent__colourName'>{colours[0].name}</span>
         </Col>
