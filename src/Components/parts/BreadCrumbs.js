0ffb874d1890d372cda1acb1b06df66401b93339
Kalovelo <apostoloskalovelonis@ieee.org>
2019-08-08 10:39:29 +0300

    fix homepage responsiveness

--- src/Components/parts/BreadCrumbs.js
+++ src/Components/parts/BreadCrumbs.js
@@ -3,30 +3,30 @@ import { Link } from 'react-router-dom';
 
 
 const BreadCrumbsLevel1 = (props) => <h3 className={'breadCrumbs-' + props.group}>
-    <Link to={'/' + props.group + 's'}>{props.group + 's'}</Link> / topics
+    <Link to='/Categories'>Categories</Link> / topics
 </h3>
 
 const BreadCrumbsLevel2 = (props) => <h3 className={'breadCrumbs-' + props.group}>
-    <Link to={'/' + props.group + 's'}>{props.group + 's'}</Link> /
+    <Link to='/Categories'>Categories</Link> /
     <Link to={'/' + props.group + 's/topics'}> topics</Link> / {props.topic}
 </h3>
 
 const BreadCrumbsLevel3 = (props) => <h3 className={'breadCrumbs-' + props.group}>
-    <Link to={'/' + props.group + 's'}>{props.group + 's'}</Link> /
+    <Link to='/Categories'>Categories</Link> /
     <Link to={'/' + props.group + 's/topics'}> topics</Link> /
     <Link to={'/' + props.group + 's/topics/' + props.topic}> {props.topic} </Link> / {props.title}
 </h3>
 
 const EventBreadCrumbsLevel1 = (props) => <h3 className={'breadCrumbs-' + props.group}>
-    <Link to={'/' + props.group + 's'}>{props.group + 's'}</Link> / archive
+    <Link to='/Categories'>Categories</Link> / archive
 </h3>
 
 const EventBreadCrumbsLevel3 = (props) => <h3 className={'breadCrumbs-' + props.group}>
-    <Link to={'/' + props.group + 's'}>{props.group + 's'}</Link> /
+    <Link to='/Categories'>Categories</Link> /
     <Link to={'/' + props.group + 's/archive'}> archive</Link> / {props.title}
 </h3>
 
 const SingleBreadCrumbs = (props) => <h3 className={'breadCrumbs-' + props.group}>
-    <Link to={props.url}>{props.parent}</Link> / {props.title}
+    <Link to='/Categories'>Categories</Link> / {props.title}
 </h3>
 export { BreadCrumbsLevel1, BreadCrumbsLevel2, BreadCrumbsLevel3, EventBreadCrumbsLevel1, EventBreadCrumbsLevel3, SingleBreadCrumbs }
