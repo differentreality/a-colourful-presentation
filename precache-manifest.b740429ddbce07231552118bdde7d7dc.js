2b27402a2cd5a9653953dcdb48bb60c131b4b84d
Prof. Apostoles <apostoloskalovelonis@gmail.com>
2019-08-10 08:35:32 +0300

    Updates

---
+++ precache-manifest.b740429ddbce07231552118bdde7d7dc.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa299e1b098f6a30a64a242d6efa4626",
+    "url": "/a-colourful-presentation/index.html"
+  },
+  {
+    "revision": "e1f78ede66e8c3a5e990",
+    "url": "/a-colourful-presentation/static/css/main.915d67ce.chunk.css"
+  },
+  {
+    "revision": "e42853f5b2ffcb14cddb",
+    "url": "/a-colourful-presentation/static/js/2.5646e0dd.chunk.js"
+  },
+  {
+    "revision": "e1f78ede66e8c3a5e990",
+    "url": "/a-colourful-presentation/static/js/main.e04ea93a.chunk.js"
+  },
+  {
+    "revision": "b777b631cb63f102372b",
+    "url": "/a-colourful-presentation/static/js/runtime~main.a815e0c9.js"
+  },
+  {
+    "revision": "8ebb74b465d8d503c0ce7e96b7669fa6",
+    "url": "/a-colourful-presentation/static/media/GSoCateith1.8ebb74b4.JPG"
+  },
+  {
+    "revision": "cbe9757bfd3dfe70f73be1e4ff9eace1",
+    "url": "/a-colourful-presentation/static/media/GSoCateith2.cbe9757b.JPG"
+  },
+  {
+    "revision": "b2014a2fa8fc05a325ad90dd8e184951",
+    "url": "/a-colourful-presentation/static/media/GSoCateith3.b2014a2f.JPG"
+  },
+  {
+    "revision": "c2832c4b0924352b9c14730fc19fd438",
+    "url": "/a-colourful-presentation/static/media/GSoCateith4.c2832c4b.JPG"
+  },
+  {
+    "revision": "d0401362e90ede7b93a0d1410ef58c06",
+    "url": "/a-colourful-presentation/static/media/GSoCateith5.d0401362.JPG"
+  },
+  {
+    "revision": "b4e70b8509fc605d06b61c7afd31d115",
+    "url": "/a-colourful-presentation/static/media/Rorateithpt2_1.b4e70b85.JPG"
+  },
+  {
+    "revision": "a6833e59f6de4a7050fafe9da1a1e397",
+    "url": "/a-colourful-presentation/static/media/Rorateithpt2_2.a6833e59.JPG"
+  },
+  {
+    "revision": "8f005786c1c41cf0e7fb88cf9072350a",
+    "url": "/a-colourful-presentation/static/media/Rorateithpt2_3.8f005786.JPG"
+  },
+  {
+    "revision": "a2b25390f8e831772fe7904356503dab",
+    "url": "/a-colourful-presentation/static/media/Rorateithpt2_4.a2b25390.JPG"
+  },
+  {
+    "revision": "68343267af4bb70b16d76d25b69c2afc",
+    "url": "/a-colourful-presentation/static/media/git101ateith.68343267.JPG"
+  },
+  {
+    "revision": "3e536d1233f7da2b27d2e4602bd7a3f3",
+    "url": "/a-colourful-presentation/static/media/git101poster.3e536d12.jpg"
+  },
+  {
+    "revision": "55e6988b283e186a42d2847885d354b3",
+    "url": "/a-colourful-presentation/static/media/gitMpratsa.55e6988b.JPG"
+  },
+  {
+    "revision": "11915b84f42eea078ff0925aacc5344f",
+    "url": "/a-colourful-presentation/static/media/gitNasia.11915b84.JPG"
+  },
+  {
+    "revision": "6ba8380cbc9c561666fe56ac8c6ab6eb",
+    "url": "/a-colourful-presentation/static/media/gitPeople.6ba8380c.JPG"
+  },
+  {
+    "revision": "d6ee985258c7ddeb3e9ea49729a71600",
+    "url": "/a-colourful-presentation/static/media/gitStella.d6ee9852.JPG"
+  },
+  {
+    "revision": "80b26ef66d2a6a3e520e366d0f393713",
+    "url": "/a-colourful-presentation/static/media/gsoc_ateith_poster.80b26ef6.jpg"
+  },
+  {
+    "revision": "f1a85bc6fc4c631af70ea36d529a7b5f",
+    "url": "/a-colourful-presentation/static/media/osem.f1a85bc6.jpg"
+  },
+  {
+    "revision": "c1ab7307843640d6b1073921c4f33ddd",
+    "url": "/a-colourful-presentation/static/media/railspt1_img1.c1ab7307.JPG"
+  },
+  {
+    "revision": "fb4f400d23b75321adfd03afccd720a5",
+    "url": "/a-colourful-presentation/static/media/railspt1_img2.fb4f400d.JPG"
+  },
+  {
+    "revision": "b5f258658a6edeb8fd8077e5c8bb393d",
+    "url": "/a-colourful-presentation/static/media/railspt1_img3.b5f25865.JPG"
+  },
+  {
+    "revision": "e374f9a8c18f3fc35b5f967ca1a65ef4",
+    "url": "/a-colourful-presentation/static/media/railspt1_img4.e374f9a8.JPG"
+  },
+  {
+    "revision": "98380109bed94c4b604502cbd88742dd",
+    "url": "/a-colourful-presentation/static/media/railspt1_img5.98380109.JPG"
+  },
+  {
+    "revision": "d56534c873fd1fc006bfde610928b0f0",
+    "url": "/a-colourful-presentation/static/media/rorpt1poster.d56534c8.jpg"
+  },
+  {
+    "revision": "2546fe1e366067e89ddfb5f9151260d3",
+    "url": "/a-colourful-presentation/static/media/rorpt2poster.2546fe1e.jpg"
+  },
+  {
+    "revision": "a75b2b69ea1135b2f5fce65cd895d35c",
+    "url": "/a-colourful-presentation/static/media/spaghetti.a75b2b69.png"
+  },
+  {
+    "revision": "b28649fd2aede062b025a7536cea594f",
+    "url": "/a-colourful-presentation/static/media/stella.b28649fd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/a-colourful-presentation/precache-manifest.4a858242cd4e2e16d8414499d059fefd.js"
+  "/a-colourful-presentation/precache-manifest.b740429ddbce07231552118bdde7d7dc.js"
 );
 
 self.addEventListener('message', (event) => {
