6de1a8347c67e6a9e4ffc53b387f86d73876ad87
Kalovelo <apostoloskalovelonis@ieee.org>
2019-08-17 23:07:31 +0300

    contact form validation

--- src/Components/pages/ContactForm.js
+++ src/Components/pages/ContactForm.js
@@ -14,8 +14,14 @@ export default class ContactForm extends React.Component {
       this.state =  {
         name: '',
         email: '',
-        subject: '',
-        message: ''
+        title: '',
+        message: '',
+        errors:{
+            name:'',
+            email:'',
+            title:'',
+            message:''
+        }
       }
 
     }
@@ -32,22 +38,30 @@ export default class ContactForm extends React.Component {
     handleSubmit(event) {
       event.preventDefault();
 
+      if(!this.validateMail())
+        return;
+
       var templateParams = {
-        "to_name": process.env.REACT_APP_TO_EMAIL,
+        "to_name": "apostoloskalovelonis@gmail.com",
         "from_name": this.state.name,
-        "subject": this.state.subject,
+        "title": this.state.title,
         "message_html": this.state.message,
       }
 
-      emailjs.send("gmail", "template_fg92o5s8", templateParams, process.env.REACT_APP_EMAILJS_KEY )
-        .then( function(resonse) {
+      //previous template id : template_fg92o5s8
+      // TODO : animation instead of toaster, with 2 colours green / red
+
+      emailjs.send("gmail", "template_JvufSO9G", templateParams, "user_uxCV2WOrhaaiiZomhv4Tb" )
+        .then( function(response) {
           toastr.success('Sent successfully!')
+        },function(err){
+            toastr.error(err)
         })
 
       this.setState({
         name: '',
         email: '',
-        subject: '',
+        title: '',
         message: ''
       })
     }
@@ -69,7 +83,6 @@ export default class ContactForm extends React.Component {
         translateY:[-100,400],
         duration:4000,
         rotate:(-30),
-        color:'red',
         complete:()=>anime({
             targets:'.animatedText',
             translateY:[400,350],
@@ -79,6 +92,49 @@ export default class ContactForm extends React.Component {
                 direction: 'alternate',
         }),
     })
+
+    validateMail=()=>{
+        let errors={};
+        let formIsValid=true;
+
+        if(!this.state.name)
+        {
+            errors.name='Name cannot be empty.';
+            formIsValid=false;
+        }
+
+        if(!this.state.title)
+        {
+            errors.title='Title cannot be empty.';
+            formIsValid=false;
+        }
+
+
+        if(!this.state.email)
+        {
+            errors.email='Mail cannot be empty.';
+            formIsValid=false;
+        }
+
+
+        if(!this.state.message)
+        {
+            errors.message='Message cannot be empty.';
+            formIsValid=false;
+        }
+
+        var mailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+
+        if(this.state.email && !mailPattern.test(this.state.email) )
+        {
+            errors.email='This is not a valid mail.'
+            formIsValid=false;
+        }
+
+        this.setState({errors:errors})
+        return formIsValid;
+    }
+
 
     render() {
         return (
@@ -94,30 +150,43 @@ export default class ContactForm extends React.Component {
                                 <h1 className='contactForm__title'>The Contact Form</h1>
 
                                 <p className='contactForm__about'>Got a question or interested in booking a workshop for your community? Send me
-                                message and you will receive an answer as soon as possible!</p>
+                                a message and you will receive an answer as soon as possible!</p>
                                 <h3 className='contactForm__subtitle'>Get to know each other</h3>
 
                                 <div className='inputBox-name'>
-                                    <label xfor="name">Name</label>
-                                    <input className='inputText' type="text" id="name" name="name" placeholder="John Smithson" onChange={this.handleInputChange.bind(this)} value={this.state.name} />
+                                    <label xfor="name">Name *</label>
+                                    <input className='inputText' 
+                                        type="text" 
+                                        id="name" 
+                                        name="name" 
+                                        placeholder="John Smithson" 
+                                        onChange={this.handleInputChange.bind(this)} 
+                                        value={this.state.name}
+                                        />
+                                        <h6 className='contactForm__error'>{this.state.errors.name}</h6>
                                 </div>
 
                                 <div className='inputBox-mail'>
-                                    <label htmlFor="mailadress">E-mail Address</label>
+                                    <label htmlFor="mailadress">E-mail Address *</label>
                                     <input className='inputText' type="text" id="mailadress" name="email" placeholder="you@mail.me" onChange={this.handleInputChange.bind(this)} value={this.state.email}/>
+                                    <h6 className='contactForm__error'>{this.state.errors.email}</h6>
                                 </div>
 
                                 <h3 className='contactForm__subtitle'>Your Message</h3>
-
                                 <div className='inputBox'>
-                                    <label xfor="subject">Title</label>
-                                    <input className='inputText' type="text" id="subject" name="subject" placeholder="Book a git workshop in Thessaloniki" onChange={this.handleInputChange.bind(this)} value={this.state.subject} required/>
+                                    <label xfor="title">Title *</label>
+                                    <input className='inputText' type="text" id="title" name="title" placeholder="Book a git workshop in Thessaloniki" onChange={this.handleInputChange.bind(this)} value={this.state.title} required/>
+                                    <h6 className='contactForm__error'>{this.state.errors.title}</h6>                                
                                 </div>
 
-                                <label xfor="message">Message</label>
+                                <label xfor="message">Message *</label>
                                 <textarea id="message" name="message" placeholder="Write something.." onChange={this.handleInputChange.bind(this)} value={this.state.message} required></textarea>
+                                <h6 className='contactForm__error'>{this.state.errors.message}</h6>
                                 <input className='contactForm__submit' type="submit" value="Submit" onClick={this.handleSubmit.bind(this)}/>
+
+
                             </form>
+
                         </Col>
                     </Row>
                 </div>
